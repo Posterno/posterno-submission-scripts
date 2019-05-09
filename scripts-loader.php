5a1b0852d73f77c60194d151dcfaf54552c4ea2f
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-09 22:37:26 +0100

    prototype assets loading

--- scripts-loader.php
+++ scripts-loader.php
@@ -10,3 +10,30 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+/**
+ * Load assets required for the frontend submission form.
+ *
+ * @return void
+ */
+function pno_load_listing_submission_form_assets() {
+
+	if ( ! is_page( pno_get_listing_submission_page_id() ) ) {
+		return;
+	}
+
+	$version = PNO_VERSION;
+	$is_dev = defined( 'PNO_VUE_DEV' ) && PNO_VUE_DEV === true ? true : false;
+
+	if ( $is_dev ) {
+
+		wp_register_script( 'pno-listing-submission-form', 'http://localhost:4001/app.js', [], $version, true );
+
+	} else {
+
+	}
+
+	wp_enqueue_script( 'pno-listing-submission-form' );
+
+}
+add_action( '$tag:string', 'pno_load_listing_submission_form_assets' );
