9f71a2d198e40ac8cdbee98f94484d1ce47a64ce
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-09 21:52:11 +0100

    prepare component for vuejs usage

---
+++ scripts-loader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Scripts and styles registration for the frontend listing submission form.
+ *
+ * @package     posterno
+ * @copyright   Copyright (c) 2018, Pressmodo, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+// Exit if accessed directly..
+defined( 'ABSPATH' ) || exit;
