fc03d1e33119bfbd3b629d857607952d36ffbe93
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-10 10:47:53 +0100

    import js vars

--- scripts-loader.php
+++ scripts-loader.php
@@ -12,6 +12,28 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
+ * Get js settings for the listings submission form.
+ *
+ * @return array
+ */
+function pno_get_listings_submission_form_js_vars() {
+
+	$js_settings = [
+		'selected_listing_type'   => isset( $_GET['listing_type_id'] ) && ! empty( sanitize_text_field( $_GET['listing_type_id'] ) ) ? absint( $_GET['listing_type_id'] ) : false, // phpcs: ignore
+		'ajax'                    => admin_url( 'admin-ajax.php' ),
+		'get_tags_nonce'          => wp_create_nonce( 'pno_get_tags_from_categories_for_submission' ),
+		'get_starter_tags_nonce'  => wp_create_nonce( 'pno_get_tags' ),
+		'get_subcategories_nonce' => wp_create_nonce( 'pno_get_subcategories' ),
+		'days'                    => pno_get_days_of_the_week(),
+		'is_editing_mode'         => is_page( pno_get_listing_editing_page_id() ),
+		'editing_listing_id'      => is_page( pno_get_listing_editing_page_id() ) && isset( $_GET['listing_id'] ) ? absint( $_GET['listing_id'] ) : false,
+	];
+
+	return apply_filters( 'pno_listings_submission_form_js_vars', $js_settings );
+
+}
+
+/**
  * Load assets required for the frontend submission form.
  *
  * @return void
@@ -25,11 +47,11 @@ function pno_load_listing_submission_form_assets() {
 	$version = PNO_VERSION;
 	$is_dev  = defined( 'PNO_VUE_DEV' ) && PNO_VUE_DEV === true ? true : false;
 
-	// Styles.
+	// Register Styles.
 	wp_register_style( 'pno-submission-select2-style', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css', false, $version );
 	wp_register_style( 'pno-submission-flatpickr-style', 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css', [], $version );
 
-	// Scripts.
+	// Register Scripts.
 	wp_register_script( 'pno-submission-select2', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js', array( 'jquery' ), $version, true );
 	wp_register_script( 'pno-submission-flatpickr', 'https://cdn.jsdelivr.net/npm/flatpickr', false, $version, true );
 
@@ -44,13 +66,17 @@ function pno_load_listing_submission_form_assets() {
 
 	}
 
+	// Load styles.
 	wp_enqueue_style( 'pno-submission-select2-style' );
 	wp_enqueue_style( 'pno-submission-flatpickr-style' );
 
+	// Load scripts.
 	wp_enqueue_script( 'pno-submission-select2' );
 	wp_enqueue_script( 'pno-submission-flatpickr' );
 	wp_enqueue_script( 'pno-vuejs' );
 	wp_enqueue_script( 'pno-listing-submission-form' );
+
+	wp_localize_script( 'pno-listing-submission-form', 'pno_submission', pno_get_listings_submission_form_js_vars() );
 
 }
 add_action( 'wp_enqueue_scripts', 'pno_load_listing_submission_form_assets', 20 );
